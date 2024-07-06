42df9046f7bf6891b8007bfbff46294ba304573c
Mahadev Maitri <mahadevmaitri@gmail.com>
2024-07-06 13:04:15 -0400

    graphql mutations creating links

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import logo from './../logo.svg';
 import './../styles/App.css';
+import CreateLink from './CreateLink';
 import LinkList from './LinkList';
 
 function App() {
   return (
-    <LinkList />
+    // <LinkList />
+    <CreateLink />
   );
 }
 
