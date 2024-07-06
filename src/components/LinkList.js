a338cbe6954eee1cbded8a833a8971ea83e9a47a
Mahadev Maitri <mahadevmaitri@gmail.com>
2024-07-06 12:37:00 -0400

    GraphQL queries feeds and links

---
+++ src/components/LinkList.js
@@ -0,0 +1,33 @@
+import Link from './Link';
+import { gql, useQuery } from '@apollo/client';
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
+
+const LinkList = () => {
+  const { data } = useQuery(FEED_QUERY);
+  return (
+    <div>
+      {data && (
+        <>
+          {data.feed.links.map((link) => (
+            <Link key={link.id} link={link} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LinkList;
