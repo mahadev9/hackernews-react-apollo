e38e1d3f7f1f1f6d9d017e235edbca3e1eedfb01
Mahadev Maitri <mahadevmaitri@gmail.com>
2024-07-06 15:24:53 -0400

    GraphQL authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
-import './../styles/App.css';
-import CreateLink from './CreateLink';
-import Header from './Header';
-import LinkList from './LinkList';
+import { Route, Routes } from "react-router-dom";
+import "./../styles/App.css";
+import CreateLink from "./CreateLink";
+import Header from "./Header";
+import LinkList from "./LinkList";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className='center w85'>
+    <div className="center w85">
       <Header />
-      <div className='ph3 pv1 background-gray'>
+      <div className="ph3 pv1 background-gray">
         <Routes>
-          <Route path='/' element={<LinkList />} />
-          <Route path='/create' element={<CreateLink />} />
+          <Route path="/" element={<LinkList />} />
+          <Route path="/create" element={<CreateLink />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,47 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { AUTH_TOKEN } from "../constants";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
-    <div className='flex pa1 justify-between nowrap orange'>
-      <div className='flex flex-fixed black'>
-        <Link to="/" className='no-underline black'>
-          <div className='fw7 mr1'>Hacker News</div>
+    <div className="flex pa1 justify-between nowrap orange">
+      <div className="flex flex-fixed black">
+        <Link to="/" className="no-underline black">
+          <div className="fw7 mr1">Hacker News</div>
         </Link>
-        <Link to="/" className='no-underline black ml1'>
+        <Link to="/" className="no-underline black ml1">
           new
         </Link>
-        <div className='ml1'>|</div>
-        <Link to="/create" className='no-underline black ml1'>
-          submit
+        <div className="ml1">|</div>
+        <Link to="/search" className="ml1 no-underline black">
+          search
         </Link>
+        {authToken && (
+          <div className="flex">
+            <div className="ml1">|</div>
+            <Link to="/create" className="ml1 no-underline black">
+              submit
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-fixed">
+        {authToken ? (
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              navigate("/");
+            }}
+          >
+            logout
+          </div>
+        ) : (
+          <Link to="/login" className="ml1 no-underline black">
+            login
+          </Link>
+        )}
       </div>
     </div>
   );
