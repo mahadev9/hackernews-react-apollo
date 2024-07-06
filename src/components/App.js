d05129a0dfe1460cb75c2a42e3f342cf6f69fcb4
Mahadev Maitri <mahadevmaitri@gmail.com>
2024-07-06 14:23:28 -0400

    GraphQL routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './../styles/App.css';
 import CreateLink from './CreateLink';
+import Header from './Header';
 import LinkList from './LinkList';
 
 function App() {
   return (
-    // <LinkList />
-    <CreateLink />
+    <div className='center w85'>
+      <Header />
+      <div className='ph3 pv1 background-gray'>
+        <Routes>
+          <Route path='/' element={<LinkList />} />
+          <Route path='/create' element={<CreateLink />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
